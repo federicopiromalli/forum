6f08138953b652f0f41c162808e15d1614c8da7c
federicopiromalli <federico.piromalli@leomail.it>
2025-05-22 12:55:11 +0200

    minor changes

--- scripts/chat-handler.js
+++ scripts/chat-handler.js
@@ -1,12 +1,31 @@
 const chat_handler = {
+  chats: [],
+  chat_item: "chats",
+  create_chats_if_needed() {
+    // crea la sezione dei messaggi
+    if (!localStorage.getItem(this.chat_item))
+      localStorage.setItem(this.chat_item, JSON.stringify([]));
+  },
+  get_chats_from_storage() {
+    // prendo le chat da localStorage
+    this.chats = JSON.parse(localStorage.getItem(this.chat_item));
+  },
+  load_chats_in_storage() {
+    // inserisco le chat dentro localStorage
+    localStorage.setItem(this.chat_item, JSON.stringify(this.chats));
+  },
   show_users(users, logged_username, div_id) {
     // mostro gli utenti nella pagina delle chat
     const div = document.getElementById(div_id);
     // se non ci sono altri utenti che quello loggato
     if (users.length === 1 && users.at(0).username === logged_username) {
-      div.innerHTML = "<h4 class='post-title'> Nessun utente! </h4>";
+      const message = document.createElement("h4");
+      message.textContent = "Nessun utente!";
+      message.className = "post-title";
+      div.appendChild(message);
       return;
     }
+    // mostro gli utenti
     users.forEach((user) => {
       if (user.username !== logged_username) {
         const button = document.createElement("button");
